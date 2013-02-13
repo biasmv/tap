373e4b83dd557ebe708d6415b2d52bfb3fd4fb26
Marco Biasini <mvbiasini@gmail.com>
2013-02-13 20:00:48 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,33 @@
 import os
 from setuptools import setup
 
+LONG_DESCRIPTION="""tap provides simple and intuitive interface to 
+deal with tabular data from text files. It offers functionality to
+manipulate, sort and visualise the data in a comprehensive manner.
+
+It is targeted at users who would like to analyse data from CSV or
+other text files. 
+"""
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "tabular data made easy",
-    version = "0.0.4",
-    author = "Tobias Schmidt and Marco Biasini",
-    author_email = "tobs@isntwork.org, mvb@isntwork.org",
-    description = ("An demonstration of how to create, document, and publish "
-                                   "to the cheese shop a5 pypi.org."),
+    name = "tap",
+    version = "1.0.0",
+    author = "Tobias Schmidt, Marco Biasini",
+    author_email = "tobias@isntwork.org, marco@isntwork.org",
+    description = ("painless tabular data management for Python"),
     license = "BSD",
-    keywords = "example documentation tutorial",
-    url = "http://packages.python.org/an_example_pypi_project",
-    packages=['table', 'tests'],
-    long_description=read('README'),
+    keywords = "table, statistics data analysis, plotting, visualisation",
+    url = "http://isntwork.org/tap",
+    packages=[ 'tap' ],
+    long_description=LONG_DESCRIPTION,
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Topic :: Utilities",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Intended Audience :: Science/Research",
+        "Environment :: Console",
         "License :: OSI Approved :: BSD License",
     ],
 )
